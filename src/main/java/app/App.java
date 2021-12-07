00ae2697bb1bb0a60427358b1856ec619c22d4db
Johan Ekman <ekman-johan020@hotmail.com>
2021-12-07 18:51:07 +0100

    Cleaned up some code

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,14 +1,17 @@
 package app;
 
+import menu.user.UserMenu;
 import model.Datasource;
 
 public class App {
     public static final Datasource DATASOURCE = new Datasource();
 
     public static void main(String[] args) {
+        DATASOURCE.open();
 //        SystemMenu systemMenu = new SystemMenu();
 //        systemMenu.start();
-
+        UserMenu userMenu = new UserMenu();
+        userMenu.start();
     }
 
 }
