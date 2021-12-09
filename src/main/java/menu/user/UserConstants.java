6a19b6c5f72f4ce892b6ee56e28363891327237a
Johan Ekman <ekman-johan020@hotmail.com>
2021-12-10 00:56:56 +0100

    Added post creation

--- src/main/java/menu/user/UserConstants.java
+++ src/main/java/menu/user/UserConstants.java
@@ -2,16 +2,7 @@ package menu.user;
 
 public enum UserConstants {
 
-    MAIN_MENU, LOGOUT, POST_MENU, CATEGORY_MENU, SEARCH_POST, ENTER_POST;
-
-    private static UserConstants userConstants = UserConstants.MAIN_MENU;
-
-    public static void setUserConstants(UserConstants userConstants) {
-        UserConstants.userConstants = userConstants;
-    }
-
-    public static UserConstants getUserConstants() {
-        return userConstants;
-    }
+    MAIN_MENU, LOGOUT, POST_MENU,
+    CATEGORY_MENU, SEARCH_POST, ENTER_POST, CREATE_POST;
 
 }
