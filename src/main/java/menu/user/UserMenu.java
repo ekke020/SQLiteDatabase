fba0819dd36b90e49cffc628657638dea48717d8
Johan Ekman <ekman-johan020@hotmail.com>
2021-12-10 00:57:27 +0100

    Added create post function

--- src/main/java/menu/user/UserMenu.java
+++ src/main/java/menu/user/UserMenu.java
@@ -1,6 +1,7 @@
 package menu.user;
 
 import app.App;
+import model.User;
 
 import java.util.Scanner;
 
@@ -10,27 +11,30 @@ import static menu.user.UserConstants.*;
 public class UserMenu implements Runnable {
 
     private final Scanner scanner = new Scanner(System.in);
+    private final User user;
     private boolean running = true;
-
+    private UserConstants userConstants = MAIN_MENU;
 
     private static final String CATEGORY_1 = "Abortions & Sins";
     private static final String CATEGORY_2 = "The classic popes";
     private static final String CATEGORY_3 = "Fun with bible";
     private static final String CATEGORY_4 = "Confessions";
 
-    public void start() {
-        new Thread(this).start();
+
+    public UserMenu(User user) {
+        this.user = user;
     }
 
     @Override
     public void run() {
         while (running) {
-            switch (getUserConstants()) {
+            switch (userConstants) {
                 case MAIN_MENU -> {
                     printMainMenu();
                     mainMenuAlternatives(scanner.nextLine());
                 }
                 case CATEGORY_MENU -> {
+                    System.out.println("CATEGORY MENU");
                     printCategoryChoices();
                     categoryAlternatives(scanner.nextLine());
                 }
@@ -40,10 +44,13 @@ public class UserMenu implements Runnable {
                 case SEARCH_POST -> {
                    searchPostByTitle();
                 }
-                case ENTER_POST -> {
-
+                case CREATE_POST -> {
+                    createNewPost();
                 }
-                case LOGOUT -> System.out.println();
+                case LOGOUT -> {
+                    App.logout(user);
+                    running = false;
+                }
             }
         }
     }
@@ -66,20 +73,43 @@ public class UserMenu implements Runnable {
     private void mainMenuAlternatives(String input) {
         switch (input) {
             case "1" -> App.DATASOURCE.queryPosts();
-            case "2" -> UserConstants.setUserConstants(UserConstants.CATEGORY_MENU);
-            case "3" -> UserConstants.setUserConstants(UserConstants.SEARCH_POST);
-            case "4" -> UserConstants.setUserConstants(UserConstants.ENTER_POST);
-            case "5" -> UserConstants.setUserConstants(UserConstants.LOGOUT);
+            case "2" -> userConstants = CATEGORY_MENU;
+            case "3" -> userConstants = SEARCH_POST;
+            case "4" -> userConstants = CREATE_POST;
+            case "5" -> {}
+            case "6" -> userConstants = LOGOUT;
         }
     }
 
     private void printCategoryChoices() {
-        System.out.println("CATEGORY MENU");
         System.out.println("\t1: Abortions & Sins");
         System.out.println("\t2: The classic popes");
         System.out.println("\t3: Fun with bible");
         System.out.println("\t4: Confessions");
         System.out.println("\t5: Back");
+    }
+
+    private void createNewPost() {
+        System.out.println("Pick a category:");
+        printCategoryChoices();
+        String category = newPostCategoryChoice(scanner.nextLine());
+        if (!category.isEmpty()) {
+            System.out.println("Enter a title for your post:\nCategory: " + category);
+            String title = scanner.nextLine();
+            App.DATASOURCE.createPost(user, category, title);
+            System.out.println("Post successfully created!");
+            userConstants = MAIN_MENU;
+        }
+    }
+
+    private String newPostCategoryChoice(String input) {
+        switch (input) {
+            case "1" -> {return CATEGORY_1;}
+            case "2" -> {return CATEGORY_2;}
+            case "3" -> {return CATEGORY_3;}
+            case "4" -> {return CATEGORY_4;}
+            default -> {userConstants = MAIN_MENU; return "";}
+        }
     }
 
     private void categoryAlternatives(String input) {
@@ -88,7 +118,8 @@ public class UserMenu implements Runnable {
             case "2" -> {App.DATASOURCE.queryPostsByCategory(CATEGORY_2);}
             case "3" -> {App.DATASOURCE.queryPostsByCategory(CATEGORY_3);}
             case "4" -> {App.DATASOURCE.queryPostsByCategory(CATEGORY_4);}
-            case "5" -> {setUserConstants(MAIN_MENU);}
+            case "5" -> {userConstants = MAIN_MENU;}
         }
     }
+
 }
