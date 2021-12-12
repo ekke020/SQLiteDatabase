66afc436b00ff7575f907a819c9cf967ffcfbf3f
Johan Ekman <ekman-johan020@hotmail.com>
2021-12-12 22:26:16 +0100

    Added all the functionality

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -1,6 +1,7 @@
 package login;
 
 import app.App;
+import menu.user.UserMenu;
 import model.User;
 
 import java.util.Scanner;
@@ -8,8 +9,25 @@ import java.util.Scanner;
 public class Login {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static Thread applicationMainThread;
 
-    public static User Login() {
+    public static void login() {
+        while (true) {
+            User user = takeCredentials();
+            if (user != null) {
+                UserMenu userMenu = new UserMenu(user);
+                System.out.println("Welcome: " + user.getUserName() + "!");
+                applicationMainThread = new Thread(userMenu);
+                applicationMainThread.start();
+                break;
+            } else {
+                System.out.println("The username or password is incorrect.");
+            }
+        }
+    }
+
+
+    private static User takeCredentials() {
         String username = enterUsername();
         String password = enterPassword();
         return App.DATASOURCE.queryLogin(username, password);
@@ -23,6 +41,12 @@ public class Login {
     private static String enterPassword() {
         System.out.println("Enter password:");
         return SCANNER.nextLine();
+    }
+
+    public static void logout(User user) {
+        System.out.println("Bye: " + user.getUserName() + "!");
+        applicationMainThread.interrupt();
+        login();
     }
 
 }
